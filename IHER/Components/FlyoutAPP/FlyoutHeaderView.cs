b88224bf3f1af6dd0189189ea445dc85b8f574da
sbaquedano <sbaquedano@bancopopular.hn>
2023-02-20 14:37:22 -0600

    Se agrego Flyout y header

---
+++ IHER/Components/FlyoutAPP/FlyoutHeaderView.cs
@@ -0,0 +1,19 @@
+namespace IHER.Components.FlyoutAPP;
+
+public class FlyoutHeaderView : ContentView
+{
+	public FlyoutHeaderView()
+	{
+        Content = new StackLayout
+        {
+            Children =
+            {
+                new Image
+                {
+                    Source="bg.png",
+                    HorizontalOptions = LayoutOptions.Center
+                }
+            }
+        };
+    }
+}
